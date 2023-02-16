389d5bd226c082402bece8c008760e621b3b00d6
PiotrZPL <zpl20052020@protonmail.ch>
2023-02-16 12:06:10 +0100

    Moved and renamed the SpecsPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:device_info_plus/device_info_plus.dart';
-import 'package:system_info2/system_info2.dart';
 
-import 'widgets/spec_table.dart';
-import 'widgets/drawer.dart';
-import 'tools/get_cpu_name.dart';
+import 'pages/specs_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,79 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Future<AndroidDeviceInfo> androidInfo = DeviceInfoPlugin().androidInfo;
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      drawer: NavigationDrawerWidget(),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: ListView(
-          children: [
-            FutureBuilder<AndroidDeviceInfo>(
-              future: androidInfo,
-              builder: (BuildContext context, AsyncSnapshot<AndroidDeviceInfo> snapshot) {
-                if (snapshot.hasData) {
-                  AndroidDeviceInfo newInfo = snapshot.data!;
-                  return SpecTable(
-                    cpuName: getCpuName(),
-                    numberOfCores: SysInfo.cores.length.toString(),
-                    cpuArchitecture: SysInfo.cores.first.architecture.toString(),
-                    // cpuProcess: SysInfo.,
-                    cpuVendor: SysInfo.cores.first.vendor,
-                    deviceModel: newInfo.model,
-                    deviceManufacturer: newInfo.manufacturer,
-                    deviceBrand: newInfo.brand,
-                    boardName: newInfo.board,
-                    hardwareName: newInfo.hardware,
-                    screenSize: ((newInfo.displayMetrics.sizeInches * 100).round() / 100).toString()
-                  );
-                }
-                else {
-                  return const Text('Calculating answer...');
-                }
-              }
-            )
-          ],
-        )
-      ),
+      home: const SpecsPage()
     );
   }
 }
---
+++ lib/pages/specs_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:system_info2/system_info2.dart';
+
+import '../widgets/spec_table.dart';
+import '../widgets/drawer.dart';
+import '../tools/get_cpu_name.dart';
+
+class SpecsPage extends StatefulWidget {
+  const SpecsPage({super.key});
+
+  @override
+  State<SpecsPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<SpecsPage> {
+  Future<AndroidDeviceInfo> androidInfo = DeviceInfoPlugin().androidInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("CustomRomApp"),
+      ),
+      drawer: NavigationDrawerWidget(),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: ListView(
+          children: [
+            FutureBuilder<AndroidDeviceInfo>(
+              future: androidInfo,
+              builder: (BuildContext context, AsyncSnapshot<AndroidDeviceInfo> snapshot) {
+                if (snapshot.hasData) {
+                  AndroidDeviceInfo newInfo = snapshot.data!;
+                  return SpecTable(
+                    cpuName: getCpuName(),
+                    numberOfCores: SysInfo.cores.length.toString(),
+                    cpuArchitecture: SysInfo.cores.first.architecture.toString(),
+                    // cpuProcess: SysInfo.,
+                    cpuVendor: SysInfo.cores.first.vendor,
+                    deviceModel: newInfo.model,
+                    deviceManufacturer: newInfo.manufacturer,
+                    deviceBrand: newInfo.brand,
+                    boardName: newInfo.board,
+                    hardwareName: newInfo.hardware,
+                    screenSize: ((newInfo.displayMetrics.sizeInches * 100).round() / 100).toString()
+                  );
+                }
+                else {
+                  return const Text('Calculating answer...');
+                }
+              }
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
